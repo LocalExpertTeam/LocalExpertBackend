06475fdd11de72e86ada1b5738fe770fcbab9f00
Pulitus <kuba.pulik@gmail.com>
2024-11-15 19:25:32 +0100

    uniformity for data in company_finder

--- src/main/java/com/local_expert/backend/company_data/company_finder/BetterCity.java
+++ src/main/java/com/local_expert/backend/company_data/company_finder/BetterCity.java
@@ -1,21 +1,15 @@
 package com.local_expert.backend.company_data.company_finder;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
-public class CompanyFinderObject {
-    private List<BetterCity> city;
-    private List<ServiceEntity> service;
-    private List<BetterScope> scope;
-
-
+public class BetterCity {
+    private String value;
+    private String tag;
 }
